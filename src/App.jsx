5317973490ddb168e6c156d58d5fa73f66ac50b6
akibkhone <akibkhone@gmail.com>
2024-03-22 04:46:25 +0530

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
 import { BrowserRouter } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import Preloader from './components/Preloader';
 
 const App = () => {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setLoading(false);
+		}, 5000);
+
+		return () => clearTimeout(timer);
+	}, []);
+
 	return (
 		<BrowserRouter>
-			<div className="relative z-0 bg-primary">
-				<div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-					<Navbar />
-					<Hero />
+			{loading ? (
+				<Preloader />
+			) : (
+				<div className="relative z-0 bg-primary">
+					<div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+						<Navbar />
+						<Hero />
+					</div>
+					<About />
+					<Experience />
+					<Tech />
+					<Works />
+					<Feedbacks />
+					<div className="relative z-0">
+						<Contact />
+						<StarsCanvas />
+					</div>
 				</div>
-				<About />
-				<Experience />
-				<Tech />
-				<Works />
-				<Feedbacks />
-				<div className="relative z-0">
-					<Contact />
-					<StarsCanvas />
-				</div>
-			</div>
+			)}
 		</BrowserRouter>
 	);
 };
