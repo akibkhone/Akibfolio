4b55b6815b139139cd439dde9c29b6dab2f88634
Akib Khan <90907996+akibkhone@users.noreply.github.com>
2024-03-23 02:07:01 +0530

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,56 +1,58 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
 const Ball = (props) => {
-	const [decal] = useTexture([props.imgUrl]);
+  const [decal, setDecal] = useState(null);
 
-	return (
-		<Float
-			speed={1.75}
-			rotationIntensity={1}
-			floatIntensity={2}>
-			<ambientLight intensity={0.25} />
-			<directionalLight position={[0, 0, 0.05]} />
-			<mesh
-				castShadow
-				receiveShadow
-				scale={[2.75, 2.75, 2.75]}>
-				<sphereGeometry args={[1, 16, 16]} />
-				<meshStandardMaterial
-					color="#fff8eb"
-					polygonOffset
-					polygonOffsetFactor={-5}
-					flatShading
-				/>
-				<Decal
-					position={[0, 0, 1]}
-					rotation={[2 * Math.PI, 0, 6.25]}
-					scale={1}
-					map={decal}
-					flatShading
-				/>
-			</mesh>
-		</Float>
-	);
+  useTexture([props.imgUrl])
+    .then((texture) => setDecal(texture))
+    .catch((err) => console.error('Failed to load texture:', err));
+
+  return decal ? (
+    <Float
+      speed={1.75}
+      rotationIntensity={1}
+      floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={[2.75, 2.75, 2.75]}>
+        <sphereGeometry args={[1, 16, 16]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
+        />
+      </mesh>
+    </Float>
+  ) : (
+    <div>Loading texture...</div>
+  );
 };
 
 const BallCanvas = ({ icon }) => {
-	return (
-		<Canvas
-			frameloop="demand"
-			dpr={[1, 2]}
-			gl={{ preserveDrawingBuffer: true }}>
-			<Suspense fallback={<CanvasLoader />}>
-				<OrbitControls enableZoom={false} />
-				<Ball imgUrl={icon} />
-			</Suspense>
-
-			<Preload all />
-		</Canvas>
-	);
+  return (
+    <Canvas
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
 };
 
 export default BallCanvas;
