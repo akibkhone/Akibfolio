df95383f8207d0ed869be20cc48bc71c9326c69f
Akib Khan <90907996+akibkhone@users.noreply.github.com>
2024-03-23 02:30:39 +0530

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,4 @@
-Import React, { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
 
