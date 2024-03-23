7e0430df13af5343901b9d5e8f2be8dcfdc90b12
akibkhone <akibkhone@gmail.com>
2024-03-23 21:52:16 +0530

    removed canvas

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,55 +1,23 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
-
-import CanvasLoader from '../Loader';
-
-const Ball = (props) => {
-	const [decal] = useTexture([props.imgUrl]);
-
-	return (
-		<Float
-			speed={1.75}
-			rotationIntensity={1}
-			floatIntensity={2}>
-			<ambientLight intensity={0.25} />
-			<directionalLight position={[0, 0, 0.05]} />
-			<mesh
-				castShadow
-				receiveShadow
-				scale={[2.75, 2.75, 2.75]}>
-				<sphereGeometry args={[1, 16, 16]} />
-				<meshStandardMaterial
-					color="#fff8eb"
-					polygonOffset
-					polygonOffsetFactor={-5}
-					flatShading
-				/>
-				<Decal
-					position={[0, 0, 1]}
-					rotation={[2 * Math.PI, 0, 6.25]}
-					scale={1}
-					map={decal}
-					flatShading
-				/>
-			</mesh>
-		</Float>
-	);
-};
+import React from 'react';
 
 const BallCanvas = ({ icon }) => {
 	return (
-		<Canvas
-			frameloop="demand"
-			dpr={[1, 2]}
-			gl={{ preserveDrawingBuffer: true }}>
-			<Suspense fallback={<CanvasLoader />}>
-				<OrbitControls enableZoom={false} />
-				<Ball imgUrl={icon} />
-			</Suspense>
-
-			<Preload all />
-		</Canvas>
+		<div style={{ position: 'relative', width: '100%', height: '100%' }}>
+			<img
+				src={icon}
+				alt="icon"
+				style={{
+					position: 'absolute',
+					top: '50%',
+					left: '50%',
+					transform: 'translate(-50%, -50%)',
+					width: '100px',
+					height: '100px',
+					borderRadius: '50%',
+					boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+				}}
+			/>
+		</div>
 	);
 };
 
