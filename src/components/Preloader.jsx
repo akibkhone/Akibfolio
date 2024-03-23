de101269104526b1f3ea5d4327477da224712d1f
akibkhone <akibkhone@gmail.com>
2024-03-23 23:01:00 +0530

    changed loader

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -4,26 +4,54 @@ import './Preloader.css'; // make sure to import the CSS file
 const Preloader = () => {
 	return (
 		<div className="preloader-container">
+			<div class="box-of-star1">
+				<div class="star star-position1"></div>
+				<div class="star star-position2"></div>
+				<div class="star star-position3"></div>
+				<div class="star star-position4"></div>
+				<div class="star star-position5"></div>
+				<div class="star star-position6"></div>
+				<div class="star star-position7"></div>
+			</div>
+			<div class="box-of-star2">
+				<div class="star star-position1"></div>
+				<div class="star star-position2"></div>
+				<div class="star star-position3"></div>
+				<div class="star star-position4"></div>
+				<div class="star star-position5"></div>
+				<div class="star star-position6"></div>
+				<div class="star star-position7"></div>
+			</div>
+			<div class="box-of-star3">
+				<div class="star star-position1"></div>
+				<div class="star star-position2"></div>
+				<div class="star star-position3"></div>
+				<div class="star star-position4"></div>
+				<div class="star star-position5"></div>
+				<div class="star star-position6"></div>
+				<div class="star star-position7"></div>
+			</div>
+			<div class="box-of-star4">
+				<div class="star star-position1"></div>
+				<div class="star star-position2"></div>
+				<div class="star star-position3"></div>
+				<div class="star star-position4"></div>
+				<div class="star star-position5"></div>
+				<div class="star star-position6"></div>
+				<div class="star star-position7"></div>
+			</div>
 			<div
-				aria-label="Orange and tan hamster running in a metal wheel"
-				role="img"
-				className="wheel-and-hamster">
-				<div className="wheel"></div>
-				<div className="hamster">
-					<div className="hamster__body">
-						<div className="hamster__head">
-							<div className="hamster__ear"></div>
-							<div className="hamster__eye"></div>
-							<div className="hamster__nose"></div>
-						</div>
-						<div className="hamster__limb hamster__limb--fr"></div>
-						<div className="hamster__limb hamster__limb--fl"></div>
-						<div className="hamster__limb hamster__limb--br"></div>
-						<div className="hamster__limb hamster__limb--bl"></div>
-						<div className="hamster__tail"></div>
-					</div>
+				data-js="astro"
+				class="astronaut">
+				<div class="head"></div>
+				<div class="arm arm-left"></div>
+				<div class="arm arm-right"></div>
+				<div class="body">
+					<div class="panel"></div>
 				</div>
-				<div className="spoke"></div>
+				<div class="leg leg-left"></div>
+				<div class="leg leg-right"></div>
+				<div class="schoolbag"></div>
 			</div>
 		</div>
 	);
