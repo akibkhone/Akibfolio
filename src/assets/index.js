cdfd3a1f5fa4496f64de502d396f520a7b950059
Akib Khan <90907996+akibkhone@users.noreply.github.com>
2024-04-03 03:35:14 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,7 @@
 import logo from './logo.png';
 import backend from './backend.png';
 import web from './web.png';
+import spring from './spring.webp';
 import github from './github.png';
 import preview from './preview.png';
 import menu from './menu.svg';
@@ -44,6 +45,7 @@ export {
 	logo,
 	backend,
 	web,
+	spring,
 	github,
 	menu,
 	close,
